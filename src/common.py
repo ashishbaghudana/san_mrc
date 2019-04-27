490e330bce17dbc69ae553d20c6ec344df4605ac
Ashish Baghudana <ashish.baghudana26@gmail.com>
2019-04-27 16:02:47 -0400

    Add relu function

--- src/common.py
+++ src/common.py
@@ -3,10 +3,15 @@ Activation and init wrapper
 Created June, 2017
 Author: xiaodl@microsoft.com
 '''
+import torch.nn.functional as F
 
 
 def linear(x):
     return x
+
+
+def relu(x):
+    return F.relu(x)
 
 
 def activation(func_a):
